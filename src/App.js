b49319677f6793b04981f9cd0487d9375e80c347
Danilo Guimarães <guimaraesdjl@gmail.com>
2018-05-03 12:35:09 -0300

    Getting started with react-i18next

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import { I18n, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <I18n ns="translations">
+        {
+          (t, { i18n }) => (
+            <div className="App">
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>{t('education.title')}</h2>
+                <button onClick={() => i18n.changeLanguage('pt-BR')}>pt-BR</button>
+                <button onClick={() => i18n.changeLanguage('en')}>en</button>
+              </div>
+              <div className="App-intro">
+                <Trans i18nKey="education.ufg">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+                </Trans>
+              </div>
+              <div>{t('description.part2')}</div>
+            </div>
+          )
+        }
+      </I18n>
     );
   }
 }
