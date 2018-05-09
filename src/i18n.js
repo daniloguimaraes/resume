ab1fcfb57923d5989948bd3213d6e1afa875f128
Danilo Guimarães <guimaraesdjl@gmail.com>
2018-05-09 17:24:47 -0300

    Moving locales path from public to src
    There was a serious problem with react-i18next combined with Github Pages: the locale resources got 404 HTTP error during the page load because the component was requesting github.io/ instead of github;io/resume. Removing the xhr-backend dependency

--- src/i18n.js
+++ src/i18n.js
@@ -1,29 +1,38 @@
-import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { reactI18nextModule } from 'react-i18next';
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { reactI18nextModule } from "react-i18next";
+
+import en from "./locales/en/translations.json";
+import ptBr from "./locales/pt-BR/translations.json";
+
+let resBundle = {
+  en: {
+    translations: en
+  },
+  "pt-BR": {
+    translations: ptBr
+  }
+};
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(reactI18nextModule)
   .init({
-    fallbackLng: 'pt-BR',
-
+    resources: resBundle,
+    fallbackLng: "pt-BR",
     // have a common namespace used around the full app
-    ns: ['translations'],
-    defaultNS: 'translations',
-
+    ns: ["translations"],
+    defaultNS: "translations",
     debug: true,
-
     interpolation: {
-      escapeValue: false, // not needed for react!!
+      escapeValue: false // not needed for react!!
     },
-
+    useLocalStorage: true,
+    wait: true,
+    initImmediate: true,
     react: {
       wait: true
     }
   });
-
 
 export default i18n;
