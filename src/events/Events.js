22eb36bf7980ce054cea7df818ff42cbe009272d
Danilo Guimarães <guimaraesdjl@gmail.com>
2018-05-14 13:17:43 -0300

    Formatting

--- src/events/Events.js
+++ src/events/Events.js
@@ -3,21 +3,21 @@ import { I18n } from "react-i18next";
 import EventsList from "./EventsList";
 
 class Events extends Component {
-    render() {
-        return(
-            <I18n ns="translations">
-            {
-                (t) => (
-                    <div className="evento">
-                        <h2>{t('events.title')} <hr/></h2>
+  render() {
+    return (
+      <I18n ns="translations">
+        {t => (
+          <div className="evento">
+            <h2>
+              {t("events.title")} <hr />
+            </h2>
 
-                        <EventsList data = {t('events.array', {returnObjects:true})} />
-                    </div>
-                )
-            }
-            </I18n>
-        );
-    }
+            <EventsList data={t("events.array", { returnObjects: true })} />
+          </div>
+        )}
+      </I18n>
+    );
+  }
 }
 
 export default Events;
--- src/personal/Personal.js
+++ src/personal/Personal.js
@@ -1,36 +1,37 @@
 import React, { Component } from "react";
 import { I18n } from "react-i18next";
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 class Personal extends Component {
+  render() {
+    return (
+      <I18n ns="translations">
+        {t => (
+          <div className="personal">
+            <h1>{t("personal.name")}</h1>
 
-    render() {
-        return(
-            <I18n ns="translations">
-            {
-                (t) => (
-                    <div className="personal">
-                        <h1>{t('personal.name')}</h1>
+            <blockquote>
+              <p>
+                {t("personal.address.title")}
+                <br />
+                E-mail:{" "}
+                <a href="mailto:guimaraes.djl@gmail.com">
+                  guimaraes.djl@gmail.com
+                </a>
+                <br />
+                Github:{" "}
+                <a href="https://github.com/daniloguimaraes">daniloguimaraes</a>
+                <br />
+                LinkedIn: <a href="">Danilo Guimarães</a>
+              </p>
+            </blockquote>
 
-                        <blockquote>
-                            <p>
-                            {t('personal.address.title')}
-                            <br/>
-                            E-mail: <a href="mailto:guimaraes.djl@gmail.com">guimaraes.djl@gmail.com</a> 
-                            <br/>
-                            Github: <a href="https://github.com/daniloguimaraes">daniloguimaraes</a>
-                            <br/>
-                            LinkedIn: <a href="">Danilo Guimarães</a>
-                            </p>
-                        </blockquote>
-                    
-                        <img src={logo} className="App-logo" alt="logo" />
-                    </div>
-                )
-            }
-            </I18n>
-        )
-    }
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        )}
+      </I18n>
+    );
+  }
 }
 
 export default Personal;
